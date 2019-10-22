79cafc43a6d4924c200f093011a16a0dd610f3ac
kovinns <kovinns@hotmail.fr>
2019-10-22 19:39:33 +0200

    Correction requette java

--- Requete.java
+++ Requete.java
@@ -14,49 +14,49 @@ import org.apache.jena.util.FileManager;
 public class Requete {
 
     static final String file  = "base.ttl";
-	
+
     public static void main (String args[]) {
     	Model model = ModelFactory.createDefaultModel();
-    	
+
         InputStream in = FileManager.get().open( file );
         if (in == null) {
             throw new IllegalArgumentException( "File: " + file + " not found");
         }
-        
+
         model.read(in, null, "Turtle");
-        
-        String prefix = "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" + 
-        		"PREFIX dbo: <http://fr.dbpedia.org/ontology/>\n" + 
-        		"PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" + 
-        		"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" + 
-        		"PREFIX wdws: <http://www.wikidata.org/wiki/Special:EntityData/>\n" + 
+
+        String prefix = "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+        		"PREFIX dbo: <http://fr.dbpedia.org/ontology/>\n" +
+        		"PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+        		"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+        		"PREFIX wdws: <http://www.wikidata.org/wiki/Special:EntityData/>\n" +
         		"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n";
-        
+
         String queryString1 = prefix + "select ?commonName\n"
         		+ "where {\n"
         		+ "?x dbo:family ?family;\n"
         		+ "   wdws:P225 ?commonName;\n"
         		+ "   wdws:P141 ?status.\n"
-        		+ "?family rdf:category ?group.\n"
+        		+ "?family ex:group ?group.\n"
         		+ "?group rdfs:label ?groupName.\n"
         		+ "filter(?groupName = \"Mammals\").\n"
         		+ "filter(?status = \"Endangered\")\n"
         		+ "} limit 100";
-    	
+
     	Query query1 = QueryFactory.create(queryString1) ;
     	try (QueryExecution qexec = QueryExecutionFactory.create(query1, model)) {
     		ResultSet results = qexec.execSelect() ;
     		ResultSetFormatter.out(System.out, results, query1) ;
 	    }
-    	
+
     	String queryString2 = prefix + "select ?familyName (count(?scientificName) as ?nbEndangered)\n"
         		+ "where {\n"
         		+ "?x rdfs:label ?scientificName;\n"
         		+ "   wdws:P141 ?status;\n"
         		+ "   dbo:family ?family.\n"
         		+ "?family rdfs:label ?familyName;\n"
-        		+ "        rdf:category ?group.\n"
-        		+ "?group rdfs:range ?type.\n"
+        		+ "        ex:group ?group.\n"
+        		+ "?group ex:type ?type.\n"
         		+ "?type rdfs:label ?typeName.\n"
         		+ "filter(?typeName = \"Plant\").\n"
         		+ "filter(?status = \"Endangered\").\n"
@@ -69,4 +69,3 @@ public class Requete {
 	    }
     }
 }
-
